c42b3db26b17423603366edcf647bb2e4198d1eb
Michael Tanczos <mike@logic-gate.com>
2026-02-12 21:54:43 -0500

    Add rzToggle Alpine source files in package lib

---
+++ packages/rizzyui/src/js/lib/components/rzToggle.js
@@ -0,0 +1,38 @@
+export default function (Alpine) {
+    Alpine.data('rzToggle', () => ({
+        pressed: false,
+        disabled: false,
+        controlled: false,
+
+        init() {
+            this.disabled = this.$el.dataset.disabled === 'true';
+            const pressedValue = this.$el.dataset.pressed;
+            this.controlled = pressedValue === 'true' || pressedValue === 'false';
+
+            if (this.controlled) {
+                this.pressed = pressedValue === 'true';
+                return;
+            }
+
+            this.pressed = this.$el.dataset.defaultPressed === 'true';
+        },
+
+        toggle() {
+            if (this.disabled) return;
+            if (this.controlled) return;
+            this.pressed = !this.pressed;
+        },
+
+        state() {
+            return this.pressed ? "on" : "off";
+        },
+
+        ariaPressed() {
+            return this.pressed.toString();
+        },
+
+        dataDisabled() {
+            return this.disabled ? "" : null;
+        }
+    }));
+}
