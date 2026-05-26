b3ff48448d544cfccfeb29debdaecbdf9b024a5f
Michael Tanczos <mike@logic-gate.com>
2026-05-26 11:47:06 -0400

    feat: Add runtime a11y primitives scaffold and conventions (#124)

---
+++ packages/rizzyui/src/js/runtime/a11y/index.js
@@ -0,0 +1,5 @@
+export { createFocusScope } from './focusScope.js';
+export { createDismissableLayer } from './dismissableLayer.js';
+export { createRovingFocusGroup } from './rovingFocusGroup.js';
+export { createTypeaheadNavigator } from './typeaheadNavigator.js';
+export { createAriaAnnouncer } from './ariaAnnouncer.js';
